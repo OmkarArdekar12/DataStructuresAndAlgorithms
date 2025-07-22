6b2245c81c6e3e0118e2680c446bd5e571c355c1
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-07-22 14:13:03 +0530

    Best Time to Buy and Sell Stock

---
+++ LeetCode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,31 @@
+// Best Time to Buy and Sell Stock
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+import java.util.*;
+import java.io.*;
+
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int buyingPrice = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i=0; i<n; ++i) {
+            int currPrice = prices[i];
+            if(currPrice < buyingPrice) {
+                buyingPrice = currPrice;
+            } else {
+                profit = Math.max(profit, currPrice - buyingPrice);
+            }
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.err.println(maxProfit(prices));
+    }
+}
