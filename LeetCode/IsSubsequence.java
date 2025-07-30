f7538fa411bfd1a51ea7002571f483f0657207db
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-07-30 13:54:09 +0530

    Is Subsequence

---
+++ LeetCode/IsSubsequence.java
@@ -0,0 +1,18 @@
+// Is Subsequence
+
+public class IsSubsequence {
+    public static boolean isSubsequence(String s, String t) {
+        int n = s.length(), m = t.length();
+        int i = 0, j = 0;
+        while(i < n && j < m) {
+            if(s.charAt(i) == t.charAt(j)) {
+                ++i;
+            }
+            ++j;
+        }
+        return i == n;
+    }
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc"));
+    }
+}
