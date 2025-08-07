f725d3dd4cd53bcd40467c8f854c47420d08b59e
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-07 21:47:20 +0530

    Word Pattern

---
+++ LeetCode/WordPattern.java
@@ -0,0 +1,53 @@
+// Word Pattern
+
+import java.util.*;
+
+public class WordPattern {
+    public static boolean wordPattern(String pattern, String s) {
+        // String strs[] = s.split(" ");
+        // int n = pattern.length(), m = strs.length;
+        // if(n != m) {
+        //     return false;
+        // }
+        // Map<Character, String> map = new HashMap<>();
+        // for(int i=0; i<n; ++i) {
+        //     char ch = pattern.charAt(i);
+        //     if(!map.containsKey(ch)) {
+        //         if(map.containsValue(strs[i])) {
+        //             return false;
+        //         }
+        //         map.put(ch, strs[i]);
+        //     } else {
+        //         if(!map.get(ch).equals(strs[i])) {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        String strs[] = s.split(" ");
+        int n = pattern.length(), m = strs.length;
+        if(n != m) {
+            return false;
+        }
+        Map<Character, String> map = new HashMap<>();
+        Map<String, Character> reverseMap = new HashMap<>();
+        for(int i=0; i<n; ++i) {
+            char ch = pattern.charAt(i);
+            if(!map.containsKey(ch)) {
+                if(reverseMap.containsKey(strs[i])) {
+                    return false;
+                }
+                map.put(ch, strs[i]);
+                reverseMap.put(strs[i], ch);
+            } else {
+                if(!map.get(ch).equals(strs[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(wordPattern("abba", "dog cat cat dog"));
+    }
+}
