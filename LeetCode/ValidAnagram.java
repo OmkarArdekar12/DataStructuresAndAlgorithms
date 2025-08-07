f8607574671391de421a5248d391291443e3337d
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-07 21:49:28 +0530

    Valid Anagram

---
+++ LeetCode/ValidAnagram.java
@@ -0,0 +1,26 @@
+// Valid Anagram
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        int n = s.length(), m = t.length();
+        if(n != m) {
+            return false;
+        }
+
+        int freq[] = new int[26];
+        for(int i=0; i<n; ++i) {
+            ++freq[s.charAt(i) - 'a'];
+            --freq[t.charAt(i) - 'a'];
+        }
+        for(int i=0; i<26; ++i) {
+            if(freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagram"));
+    }
+}
