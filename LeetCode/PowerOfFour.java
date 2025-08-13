221ec1d286d77a75c315d668c6485788e39208ff
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-13 20:32:06 +0530

    Power Of Four

---
+++ LeetCode/PowerOfFour.java
@@ -0,0 +1,23 @@
+// Power of Four
+
+public class PowerOfFour {
+    public static boolean isPowerOfFour(int n) {
+        // if(n <= 0) {
+        //     return false;
+        // }
+        // while(n % 4 == 0) {
+        //     n /= 4;
+        // }
+        // return n == 1;
+
+        if(n <= 0) {
+            return false;
+        }
+        return (Math.log10(n) / Math.log10(4)) % 1 == 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 16;
+        System.out.println(isPowerOfFour(n));
+    }
+}
