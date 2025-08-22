249735e1a284d23364924f874b4a7648c6931ed1
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-22 23:59:30 +0530

    Array Coloring

--- Codeforces/ArrayColoring.java
+++ Codeforces/ArrayColoring.java
@@ -5,6 +5,21 @@ import java.io.*;
 
 public class ArrayColoring {
 	public static void main(String args[]) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        PrintWriter pw = new PrintWriter(System.out);
+        for(int x=0; x<t; ++x) {
+            int n = Integer.parseInt(br.readLine());
+            int a[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Arrays.parallelSort(a);
+            int res = 0;
+            for(int i=0, j=n-1; i<j; ++i, --j) {
+                res += a[j] - a[i];
+            }
+            pw.println(res);
+        }
+        pw.close();
+        br.close();
         // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // int t = Integer.parseInt(br.readLine());
         // PrintWriter pw = new PrintWriter(System.out);
@@ -21,20 +36,5 @@ public class ArrayColoring {
         // pw.flush();
         // pw.close();
         // br.close();
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        PrintWriter pw = new PrintWriter(System.out);
-        for(int x=0; x<t; ++x) {
-            int n = Integer.parseInt(br.readLine());
-            int a[] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            Arrays.parallelSort(a);
-            int res = 0;
-            for(int i=0, j=n-1; i<j; ++i, --j) {
-                res += a[j] - a[i];
-            }
-            pw.println(res);
-        }
-        pw.close();
-        br.close();
 	}
 }
