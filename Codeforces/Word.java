5dccf4dd441e4c65ce0d0f84d85a84da0230eaaa
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-22 23:54:45 +0530

    Word

---
+++ Codeforces/Word.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Word {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+
+		int n = s.length();
+		int lower = 0, upper = 0;
+		for(int i=0; i<n; ++i) {
+			char ch = s.charAt(i);
+			if('a' <= ch && ch <= 'z') {
+				++lower;
+			} else {
+				++upper;
+			}
+		}
+
+		String res = "";
+		if(lower >= upper) {
+			res = s.toLowerCase();
+		} else {
+			res = s.toUpperCase();
+		}
+
+		System.out.println(res);
+	}
+}
