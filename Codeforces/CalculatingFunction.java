22b772ff911c52b05e2c94f914f764c0b39ccf56
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-24 21:43:15 +0530

    Calculating Function

---
+++ Codeforces/CalculatingFunction.java
@@ -0,0 +1,21 @@
+// Calculating Function 486-A
+
+import java.util.*;
+
+public class CalculatingFunction {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // long n = sc.nextLong();
+        // long oddSum = ((n + 1) / 2) * ((n + 1) / 2);
+        // long evenSum = (n / 2) * ((n / 2) + 1);
+        // long res = evenSum - oddSum;
+        // System.out.println(res);
+        // sc.close();
+
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long res = n % 2 == 0 ? n / 2 : -(n + 1) / 2;
+        System.out.println(res);
+        sc.close();
+    }
+}
