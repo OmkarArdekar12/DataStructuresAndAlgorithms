0254f376fdc1943f897956924692db14c7affd1e
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-25 20:20:24 +0530

    Factorial Zeroes

---
+++ Codeforces/FactorialZeroes.java
@@ -0,0 +1,16 @@
+// Factorial Zeroes
+
+import java.util.Scanner;
+
+public class FactorialZeroes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int res = 0;
+		for(int i=5; i<=n; i*=5) {
+			res += n / i;
+		}
+		System.out.println(res);
+		sc.close();
+	}
+}
