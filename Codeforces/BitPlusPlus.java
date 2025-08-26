a70badacf6dd36270afd9e61b7c809d54a2277da
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-26 21:25:16 +0530

    Bit++

---
+++ Codeforces/BitPlusPlus.java
@@ -0,0 +1,21 @@
+// Bit++ 282-A
+
+import java.io.*;
+
+public class BitPlusPlus {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int x = 0;
+		for(int i=0; i<n; ++i) {
+			String s = br.readLine();
+
+			if(s.contains("++")) {
+				++x;
+			} else if(s.contains("--")) {
+				--x;
+			}
+		}
+		System.out.println(x);
+	}
+}
