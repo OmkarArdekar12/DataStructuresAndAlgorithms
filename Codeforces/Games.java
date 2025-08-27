59910905c0daa3236607b56d12ed4c3109bcc300
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-27 16:43:36 +0530

    Games

---
+++ Codeforces/Games.java
@@ -0,0 +1,29 @@
+// Games
+
+import java.util.*;
+import java.io.*;
+
+public class Games {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int home[] = new int[n];
+        int guest[] = new int[n];
+        for(int i=0; i<n; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            home[i] = Integer.parseInt(st.nextToken());
+            guest[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int res = 0;
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<n; ++j) {
+                if(i != j && home[i] == guest[j]) {
+                    ++res;
+                }
+            }
+        }
+        
+        System.out.println(res);
+    }
+}*/
