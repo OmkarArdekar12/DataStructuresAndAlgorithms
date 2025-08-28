7b22822efda49af4a2bde61e4d9bf1c60a306000
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-28 10:57:08 +0530

    Fall Down

--- Codeforces/FallDown.java
+++ Codeforces/FallDown.java
@@ -1,67 +1,67 @@
 // Fall Down 1669-G
 
+// import java.util.*;
+
+// public class FallDown {
+// 	public static void fallDown(int n, int m, char grid[][]) {
+// 		char res[][] = new char[n][m];
+// 		for(int j=0; j<m; ++j) {
+// 			StringBuilder sb = new StringBuilder("");
+// 			for(int i=0; i<n; ++i) {
+// 				char ch = grid[i][j];
+// 				if(ch == '.') {
+// 					int k = sb.length() - 1;
+// 					if(sb.length() == 0 || sb.charAt(k) != '*') {
+// 						sb.append(ch);
+// 						continue;
+// 					}
+// 					while(k >= 0 && sb.charAt(k) == '*') {
+// 						--k;
+// 					}
+// 					sb.insert(k+1, ch);
+// 				} else {
+// 					sb.append(ch);
+// 				}
+// 			}
+
+// 			for(int i=0; i<n; ++i) {
+// 				res[i][j] = sb.charAt(i);
+// 			}
+// 		}
+
+// 		for(int i=0; i<n; ++i) {
+// 			for(int j=0; j<m; ++j) {
+// 				System.out.print(res[i][j]);
+// 			}
+// 			System.out.println();
+// 		}
+// 		System.out.println();
+// 	}
+
+// 	public static void main(String[] args) {
+// 		Scanner sc = new Scanner(System.in);
+// 		int t = sc.nextInt();
+// 		for(int k=0; k<t; ++k) {
+// 			int n = sc.nextInt();
+// 			int m = sc.nextInt();
+// 			char grid[][] = new char[n][m];
+// 			for(int i=0; i<n; ++i) {
+// 				String s = sc.next();
+// 				for(int j=0; j<m; ++j) {
+// 					grid[i][j] = s.charAt(j);
+// 				}
+// 			}
+// 			fallDown(n, m, grid);
+// 		}
+// 		sc.close();
+// 	}
+// }
+
+//Optimized Solution
+import java.io.*;
 import java.util.*;
 
 public class FallDown {
-	public static void fallDown(int n, int m, char grid[][]) {
-		char res[][] = new char[n][m];
-		for(int j=0; j<m; ++j) {
-			StringBuilder sb = new StringBuilder("");
-			for(int i=0; i<n; ++i) {
-				char ch = grid[i][j];
-				if(ch == '.') {
-					int k = sb.length() - 1;
-					if(sb.length() == 0 || sb.charAt(k) != '*') {
-						sb.append(ch);
-						continue;
-					}
-					while(k >= 0 && sb.charAt(k) == '*') {
-						--k;
-					}
-					sb.insert(k+1, ch);
-				} else {
-					sb.append(ch);
-				}
-			}
-
-			for(int i=0; i<n; ++i) {
-				res[i][j] = sb.charAt(i);
-			}
-		}
-
-		for(int i=0; i<n; ++i) {
-			for(int j=0; j<m; ++j) {
-				System.out.print(res[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for(int k=0; k<t; ++k) {
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			char grid[][] = new char[n][m];
-			for(int i=0; i<n; ++i) {
-				String s = sc.next();
-				for(int j=0; j<m; ++j) {
-					grid[i][j] = s.charAt(j);
-				}
-			}
-			fallDown(n, m, grid);
-		}
-		sc.close();
-	}
-}
-
-//Optimized Solution
-/*import java.io.*;
-import java.util.*;
-
-public class FallDownOptimized {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -105,56 +105,56 @@ public class FallDownOptimized {
 }
 
 //Optimized Solution
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class FallDownOptimized {
-    public static void fallDown(int n, int m, char[][] grid) {
-        // Process each column separately
-        for (int j = 0; j < m; j++) {
-            // The position where the next stone should fall
-            int bottom = n - 1;
+// public class FallDownOptimized {
+//     public static void fallDown(int n, int m, char[][] grid) {
+//         // Process each column separately
+//         for (int j = 0; j < m; j++) {
+//             // The position where the next stone should fall
+//             int bottom = n - 1;
             
-            // Process column from bottom to top
-            for (int i = n - 1; i >= 0; i--) {
-                if (grid[i][j] == '*') {
-                    // If it's not already at the bottom position, move it down
-                    if (i != bottom) {
-                        grid[bottom][j] = '*';
-                        grid[i][j] = '.';
-                    }
-                    bottom--;
-                } else if (grid[i][j] == 'o') {
-                    // Obstacle - reset the bottom position
-                    bottom = i - 1;
-                }
-            }
-        }
+//             // Process column from bottom to top
+//             for (int i = n - 1; i >= 0; i--) {
+//                 if (grid[i][j] == '*') {
+//                     // If it's not already at the bottom position, move it down
+//                     if (i != bottom) {
+//                         grid[bottom][j] = '*';
+//                         grid[i][j] = '.';
+//                     }
+//                     bottom--;
+//                 } else if (grid[i][j] == 'o') {
+//                     // Obstacle - reset the bottom position
+//                     bottom = i - 1;
+//                 }
+//             }
+//         }
         
-        // Print the result
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(grid[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
+//         // Print the result
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 System.out.print(grid[i][j]);
+//             }
+//             System.out.println();
+//         }
+//         System.out.println();
+//     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int k = 0; k < t; k++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            char[][] grid = new char[n][m];
-            for (int i = 0; i < n; i++) {
-                String s = sc.next();
-                for (int j = 0; j < m; j++) {
-                    grid[i][j] = s.charAt(j);
-                }
-            }
-            fallDown(n, m, grid);
-        }
-        sc.close();
-    }
-}*/
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int t = sc.nextInt();
+//         for (int k = 0; k < t; k++) {
+//             int n = sc.nextInt();
+//             int m = sc.nextInt();
+//             char[][] grid = new char[n][m];
+//             for (int i = 0; i < n; i++) {
+//                 String s = sc.next();
+//                 for (int j = 0; j < m; j++) {
+//                     grid[i][j] = s.charAt(j);
+//                 }
+//             }
+//             fallDown(n, m, grid);
+//         }
+//         sc.close();
+//     }
+// }
