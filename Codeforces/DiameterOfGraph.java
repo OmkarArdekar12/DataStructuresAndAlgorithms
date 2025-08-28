6ba14561b0b9e330b3e1a5230c0e4d9d0eee500e
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-28 10:51:42 +0530

    Diameter of Graph

---
+++ Codeforces/DiameterOfGraph.java
@@ -0,0 +1,35 @@
+// Diameter of Graph 1581-B
+
+import java.util.Scanner;
+
+public class DiameterOfGraph {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            long n = sc.nextLong();
+            long m = sc.nextLong();
+            long k = sc.nextLong();
+            k -= 2;
+            if(m < n - 1 || m > n * (n - 1) / 2) {
+                System.out.println("NO");
+            } else if(k < 0) {
+                System.out.println("NO");
+            } else if(k == 0) {
+                if(n == 1) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            } else if(k == 1) {
+                if(m == n * (n - 1) / 2) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            } else {
+                System.out.println("YES");
+            }
+        }
+    }
+}
