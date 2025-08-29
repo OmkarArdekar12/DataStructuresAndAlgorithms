83d57dacb06e4be517bcff4e5c21c95cd9539077
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-29 10:13:24 +0530

    Love Song

---
+++ Codeforces/LoveSong.java
@@ -0,0 +1,30 @@
+// Love Song
+
+import java.io.*;
+import java.util.*;
+
+public class LoveSong {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        String s = br.readLine();
+
+        long[] pref = new long[n+1];
+        for(int i=0; i<n; ++i) {
+            int value = (s.charAt(i) - 'a') + 1;
+            pref[i+1] = pref[i] + value;
+        }
+        
+        for(int i=0; i<q; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            pw.println(pref[r] - pref[l-1]);
+        }
+        pw.close();
+        br.close();
+    }
+}
