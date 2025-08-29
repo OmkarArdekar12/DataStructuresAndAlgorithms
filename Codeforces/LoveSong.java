68c0457b36e351bb9e03df3bf17d3152f66e860a
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-08-29 10:23:08 +0530

    Love Song

--- Codeforces/LoveSong.java
+++ Codeforces/LoveSong.java
@@ -14,17 +14,18 @@ public class LoveSong {
 
         long[] prefix = new long[n+1];
         for(int i=0; i<n; ++i) {
-            int value = (s.charAt(i) - 'a') + 1;
-            prefix[i+1] = prefix[i] + value;
+            int currVal = (s.charAt(i) - 'a') + 1;
+            prefix[i+1] = prefix[i] + currVal;
         }
         
         for(int i=0; i<q; ++i) {
             st = new StringTokenizer(br.readLine());
             int l = Integer.parseInt(st.nextToken());
             int r = Integer.parseInt(st.nextToken());
-            pw.println(prefix[r] - prefix[l-1]);
+            long curr = prefix[r] - prefix[l - 1];
+            pw.println(curr);
         }
-        
+
         pw.flush();
         pw.close();
         br.close();
