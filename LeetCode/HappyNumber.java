afd3ffe46a14d7110a56384a8927d416b8e98ba4
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-01 21:57:55 +0530

    Happy Number

--- LeetCode/HappyNumber.java
+++ LeetCode/HappyNumber.java
@@ -2,14 +2,6 @@
 
 public class HappyNumber {
     public static int sqSum(int x) {
-        // int sum = 0;
-        // while(x != 0) {
-        //     int lastDigit = x % 10;
-        //     sum += (lastDigit * lastDigit);
-        //     x /= 10;
-        // }
-        // return sum;
-
         int sum = 0;
         while(x > 0) {
             int ld = x % 10;
@@ -17,35 +9,43 @@ public class HappyNumber {
             x /= 10;
         }
         return sum;
+
+        // int sum = 0;
+        // while(x != 0) {
+        //     int lastDigit = x % 10;
+        //     sum += (lastDigit * lastDigit);
+        //     x /= 10;
+        // }
+        // return sum;
     }
     public static boolean isHappy(int n) {
-        // int slow = n, fast = n;
-        // do {
-        //     slow = sqSum(slow);
-        //     fast = sqSum(sqSum(fast));
-        // } while(slow != fast);
-        // return slow == 1;
-
-        // int slow = n, fast = sqSum(n);
-        // while(slow != fast) {
-        //     slow = sqSum(slow);
-        //     fast = sqSum(sqSum(fast));
-        // }
-        // return slow == 1;
-
-        // int slow = n, fast = sqSum(n);
-        // while(fast != 1 && slow != fast) {
-        //     slow = sqSum(slow);
-        //     fast = sqSum(sqSum(fast));
-        // }
-        // return fast == 1;
-
         int slow = n, fast = sqSum(n);
         while(slow != 1 && slow != fast) {
             slow = sqSum(slow);
             fast = sqSum(sqSum(fast));
         }
         return slow == 1;
+        
+        // int slow = n, fast = n;
+        // do {
+        //     slow = sqSum(slow);
+        //     fast = sqSum(sqSum(fast));
+        // } while(slow != fast);
+        // return slow == 1;
+    
+        // int slow = n, fast = sqSum(n);
+        // while(slow != fast) {
+        //     slow = sqSum(slow);
+        //     fast = sqSum(sqSum(fast));
+        // }
+        // return slow == 1;
+    
+        // int slow = n, fast = sqSum(n);
+        // while(fast != 1 && slow != fast) {
+        //     slow = sqSum(slow);
+        //     fast = sqSum(sqSum(fast));
+        // }
+        // return fast == 1;
     }
 
     public static void main(String[] args) {
