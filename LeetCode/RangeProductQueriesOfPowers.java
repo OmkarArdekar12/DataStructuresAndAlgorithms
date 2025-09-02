10f747be6955b9549f5ae02f65e1196c972fbf75
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-02 18:20:11 +0530

    Range Product Queries of Powers

--- LeetCode/RangeProductQueriesOfPowers.java
+++ LeetCode/RangeProductQueriesOfPowers.java
@@ -5,6 +5,38 @@ import java.util.*;
 public class RangeProductQueriesOfPowers {
     static final int mod = 1000000007;
     public static int[] productQueries(int n, int[][] queries) {
+        
+        //TC = O(log^2(n) + q)
+        int m = queries.length;
+        int p = Integer.bitCount(n);
+        int powers[] = new int[p];
+        int x = n, powerOf2 = 1, idx = 0;
+        while(x > 0) {
+            int bit = x % 2;
+            if(bit == 1) {
+                powers[idx] = powerOf2;
+                ++idx;
+            }
+            powerOf2 *= 2;
+            x /= 2;
+        }
+        
+        int prefix[][] = new int[p][p];
+        for(int i=0; i<p; ++i) {
+            long curr = 1;
+            for(int j=i; j<p; ++j) {
+                curr = (curr * powers[j]) % mod;
+                prefix[i][j] = (int)(curr);
+            }
+        }
+        
+        int res[] = new int[m];
+        for(int i=0; i<m; ++i) {
+            int left = queries[i][0], right = queries[i][1];
+            res[i] = prefix[left][right];
+        }
+        return res;
+        
         // TC = O(q * log(n))
         // int m = queries.length;
         // List<Integer> powers = new ArrayList<>();
@@ -57,37 +89,6 @@ public class RangeProductQueriesOfPowers {
         //     res[i] = prefix[left][right];
         // }
         // return res;
-
-        //TC = O(log^2(n) + q)
-        int m = queries.length;
-        int p = Integer.bitCount(n);
-        int powers[] = new int[p];
-        int x = n, powerOf2 = 1, idx = 0;
-        while(x > 0) {
-            int bit = x % 2;
-            if(bit == 1) {
-                powers[idx] = powerOf2;
-                ++idx;
-            }
-            powerOf2 *= 2;
-            x /= 2;
-        }
-
-        int prefix[][] = new int[p][p];
-        for(int i=0; i<p; ++i) {
-            long curr = 1;
-            for(int j=i; j<p; ++j) {
-                curr = (curr * powers[j]) % mod;
-                prefix[i][j] = (int)(curr);
-            }
-        }
-
-        int res[] = new int[m];
-        for(int i=0; i<m; ++i) {
-            int left = queries[i][0], right = queries[i][1];
-            res[i] = prefix[left][right];
-        }
-        return res;
     }
 
     public static void main(String[] args) {
