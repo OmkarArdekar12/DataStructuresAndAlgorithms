9646ae1b4231514b40d4bc7cf4309678e6f8390d
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-02 18:19:08 +0530

    Power of Two

--- LeetCode/PowerOfTwo.java
+++ LeetCode/PowerOfTwo.java
@@ -2,6 +2,11 @@
 
 public class PowerOfTwo {
     public static boolean isPowerOfTwo(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        return Integer.bitCount(n) == 1;
+
         // if(n <= 0) {
         //     return false;
         // }
@@ -14,11 +19,6 @@ public class PowerOfTwo {
         //     return false;
         // }
         // return (Math.log10(n) / Math.log10(2)) % 1 == 0;
-
-        if(n <= 0) {
-            return false;
-        }
-        return Integer.bitCount(n) == 1;
     }
 
     public static void main(String[] args) {
