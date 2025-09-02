b8c0ec3616b659cceb367be190086131a70cc171
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-02 18:14:08 +0530

    Longest Increasing Subseuence

--- LeetCode/LongestIncreasingSubsequence.java
+++ LeetCode/LongestIncreasingSubsequence.java
@@ -5,22 +5,6 @@ import java.util.*;
 
 public class LongestIncreasingSubsequence {
     public static int lengthOfLIS(int[] nums) {
-        // int n = nums.length;
-        // int dp[] = new int[n];
-        // for(int i=0; i<n; ++i) {
-        //     dp[i] = 1;
-        // }
-        // int res = 1;
-        // for(int i=1; i<n; ++i) {
-        //     for(int j=0; j<i; ++j) {
-        //         if(nums[i] > nums[j]) {
-        //             dp[i] = Math.max(dp[i], dp[j] + 1);
-        //         }
-        //     }
-        //     res = Math.max(res, dp[i]);
-        // }
-        // return res;
-
         int n = nums.length;
         List<Integer> subsequence = new ArrayList<>();
         for(int i=0; i<n; ++i) {
@@ -41,8 +25,24 @@ public class LongestIncreasingSubsequence {
             }
         }
         int res = subsequence.size();
-
+        
         return res;
+
+        // int n = nums.length;
+        // int dp[] = new int[n];
+        // for(int i=0; i<n; ++i) {
+        //     dp[i] = 1;
+        // }
+        // int res = 1;
+        // for(int i=1; i<n; ++i) {
+        //     for(int j=0; j<i; ++j) {
+        //         if(nums[i] > nums[j]) {
+        //             dp[i] = Math.max(dp[i], dp[j] + 1);
+        //         }
+        //     }
+        //     res = Math.max(res, dp[i]);
+        // }
+        // return res;
     }
 
     public static void main(String[] args) {
