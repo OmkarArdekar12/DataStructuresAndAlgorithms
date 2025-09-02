7622cd66ba28ff311dc0f6fa274b56cbad2374bc
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-02 18:15:36 +0530

    Longest Substring Without Repeating Characters

--- LeetCode/LongestSubstringWithoutRepeatingCharacters.java
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters.java
@@ -2,16 +2,6 @@
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public static int lengthOfLongestSubstring(String s) {
-        // int n = s.length();
-        // int res = 0;
-        // Map<Character, Integer> map = new HashMap<>();
-        // for(int i=0, j=0; j<n; ++j) {
-        //     char ch = s.charAt(j);
-        //     i = Math.max(i, map.getOrDefault(ch, -1) + 1);
-        //     res = Math.max(res, j - i + 1);
-        //     map.put(ch, j);
-        // }
-        // return res;
         int n = s.length();
         int res = 0;
         int asciiLastSeenIdx[] = new int[128];
@@ -22,6 +12,17 @@ public class LongestSubstringWithoutRepeatingCharacters {
             asciiLastSeenIdx[ch] = j + 1;
         }
         return res;
+        
+        // int n = s.length();
+        // int res = 0;
+        // Map<Character, Integer> map = new HashMap<>();
+        // for(int i=0, j=0; j<n; ++j) {
+        //     char ch = s.charAt(j);
+        //     i = Math.max(i, map.getOrDefault(ch, -1) + 1);
+        //     res = Math.max(res, j - i + 1);
+        //     map.put(ch, j);
+        // }
+        // return res;
     }
     public static void main(String[] args) {
         String s = "abcabcbb";
