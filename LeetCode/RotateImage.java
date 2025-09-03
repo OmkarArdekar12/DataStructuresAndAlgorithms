a4baea8e4d5aa585760f77f1f5aa01b76283e84b
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-03 15:16:20 +0530

    Rotate Image

--- LeetCode/RotateImage.java
+++ LeetCode/RotateImage.java
@@ -4,6 +4,23 @@ import java.util.*;
 
 public class RotateImage {
     public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0; i<n; ++i) {
+            for(int j=i; j<n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<n/2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+        return;
+        
         // int n = matrix.length;
         // for(int i=0; i<n; ++i) {
         //     for(int j=0; j<i; ++j) {
@@ -22,24 +39,8 @@ public class RotateImage {
         //     k++;
         // }
         // return;
-        int n = matrix.length;
-        for(int i=0; i<n; ++i) {
-            for(int j=i; j<n; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-        for(int i=0; i<n; ++i) {
-            for(int j=0; j<n/2; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-1-j];
-                matrix[i][n-1-j] = temp;
-            }
-        }
-        return;
     }
-
+    
     public static void main(String[] args) {
         int matrix[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
         for(int r[] : matrix) {
