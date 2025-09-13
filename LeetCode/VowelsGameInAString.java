ca13157ef12f0f0d534ffbfbd0a0a8fa1dbb83e6
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-09-13 22:26:45 +0530

    Vowels Game in a String

--- LeetCode/VowelsGameInAString.java
+++ LeetCode/VowelsGameInAString.java
@@ -3,7 +3,6 @@
 public class VowelsGameInAString {
     public static boolean doesAliceWin(String s) {
         int n = s.length();
-        int vowelsCount = 0;
         for(int i=0; i<n; ++i) {
             char c = s.charAt(i);
             if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
@@ -15,7 +14,7 @@ public class VowelsGameInAString {
     }
 
     public static void main(String[] args) {
-        String s = "abcdieo";
+        String s = "leetcode";
         System.out.println(doesAliceWin(s));
     }
 }
